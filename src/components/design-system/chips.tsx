74efff25c40189e1dfc12de407311672b78efcde
shristigupta12 <shristiguptawork@gmail.com>
2024-11-30 00:20:13 +0530

    replace: title with search in filter

--- src/components/design-system/chips.tsx
+++ src/components/design-system/chips.tsx
@@ -151,7 +151,7 @@ export const FilterChips = ({
             </PopoverTrigger>
             <PopoverContent className="w-[200px] p-0" align="start">
                 <Command>
-                    <CommandInput placeholder="title" />
+                    <CommandInput placeholder="Search" />
                     <CommandList>
                         <CommandEmpty>filters_no_results_found</CommandEmpty>
                         <CommandGroup>
