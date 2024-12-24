4bd6f39908d51625389972afbb72d1a607a11079
shristigupta12 <shristiguptawork@gmail.com>
2024-12-24 15:24:33 +0530

    added the api endpoint for student update operations

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -11,3 +11,4 @@ export const INIT_INSTITUTE = `${BASE_URL}/admin-core-service/institute/v1/detai
 export const GET_STUDENTS = `${BASE_URL}/admin-core-service/institute/student/get/v1/all`;
 export const ENROLL_STUDENT_MANUALLY = `${BASE_URL}/admin-core-service/institute/student/v1/add-student`;
 export const INIT_CSV_BULK = `${BASE_URL}/admin-core-service/institute/student-bulk/v1/init-student-upload`;
+export const STUDENT_UPDATE_OPERATION = `${BASE_URL}/admin-core-service/institute/student-operation/v1/update`;
