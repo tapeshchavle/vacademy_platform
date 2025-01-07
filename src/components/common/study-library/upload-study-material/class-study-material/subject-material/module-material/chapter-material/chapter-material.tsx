e023b1409430134ce3a1a145fc34d9c432c27c55
shristigupta12 <shristiguptawork@gmail.com>
2025-01-07 11:33:59 +0530

    added heading in slide page

--- src/components/common/study-library/upload-study-material/class-study-material/subject-material/module-material/chapter-material/chapter-material.tsx
+++ src/components/common/study-library/upload-study-material/class-study-material/subject-material/module-material/chapter-material/chapter-material.tsx
@@ -1,37 +1,3 @@
-// import { useMemo, useState } from "react";
-// import YooptaEditor, { createYooptaEditor, YooEditor, YooptaContentValue } from "@yoopta/editor";
-// import Paragraph from "@yoopta/paragraph";
-// import Image from "@yoopta/image";
-
-// const plugins = [Paragraph,
-//     Image
-// ];
-
-// export const ChapterMaterial = () => {
-//     const editor: YooEditor = useMemo(() => createYooptaEditor(), []);
-//     const [value, setValue] = useState<YooptaContentValue>();
-
-//     //   const onChange = (value: YooptaContentValue, options: YooptaOnChangeOptions) => {
-//     //     setValue(value);
-//     //   };
-
-//     const onChange = (value: YooptaContentValue) => {
-//         setValue(value);
-//     };
-
-//     return (
-//         <div>
-//             <YooptaEditor
-//                 editor={editor}
-//                 plugins={plugins}
-//                 placeholder="Type something"
-//                 value={value}
-//                 onChange={onChange}
-//             />
-//         </div>
-//     );
-// };
-
 import YooptaEditor, {
     createYooptaEditor,
     // Elements,
@@ -62,6 +28,8 @@ import LinkTool, { DefaultLinkToolRender } from "@yoopta/link-tool";
 
 //   import { uploadToCloudinary } from '@/utils/cloudinary';
 import { useMemo, useRef } from "react";
+import { MyButton } from "@/components/design-system/button";
+import { DotsThree } from "@phosphor-icons/react";
 //   import { WITH_BASIC_INIT_VALUE } from './initValue';
 
 const plugins = [
@@ -112,19 +80,31 @@ export const ChapterMaterial = () => {
     // };
 
     return (
-        <div
-            className="flex justify-center bg-primary-50 px-[20px] pb-[40px] pt-[80px] md:py-[100px] md:pl-[200px] md:pr-[80px]"
-            ref={selectionRef}
-        >
-            <YooptaEditor
-                editor={editor}
-                plugins={plugins}
-                tools={TOOLS}
-                marks={MARKS}
-                selectionBoxRoot={selectionRef}
-                //   onChange={onChange}
-                autoFocus
-            />
+        <div className="flex w-full flex-col" ref={selectionRef}>
+            <div className="-mx-8 -my-8 flex items-center justify-between gap-6 border-b border-neutral-300 px-8 py-4">
+                <h3 className="text-h3 font-semibold text-neutral-600">
+                    Understanding the Human Eye
+                </h3>
+                <div className="flex items-center gap-6">
+                    <MyButton buttonType="secondary" scale="large" layoutVariant="default">
+                        Edit
+                    </MyButton>
+                    <MyButton buttonType="secondary" scale="large" layoutVariant="icon">
+                        <DotsThree />
+                    </MyButton>
+                </div>
+            </div>
+            <div className="mt-14 h-full w-full px-10">
+                <YooptaEditor
+                    editor={editor}
+                    plugins={plugins}
+                    tools={TOOLS}
+                    marks={MARKS}
+                    selectionBoxRoot={selectionRef}
+                    //   onChange={onChange}
+                    autoFocus
+                />
+            </div>
         </div>
     );
 };
