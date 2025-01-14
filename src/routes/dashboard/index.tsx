f236f77d796bea2bc8be6aa3c8b8d281ee5fc5fa
shristigupta12 <shristiguptawork@gmail.com>
2025-01-14 19:25:54 +0530

    fixed the fetching issue of init institute api data

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,10 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { LayoutContainer } from "@/components/common/layout-container/layout-container";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useInstituteQuery } from "@/services/student-list-section/getInstituteDetails";
+import { useEffect } from "react";
+import { useInstituteDetailsStore } from "@/stores/students/students-list/useInstituteDetailsStore";
 
 export const Route = createFileRoute("/dashboard/")({
     component: Dashboard,
 });
 
 export function Dashboard() {
+    const { data } = useSuspenseQuery(useInstituteQuery());
+
+    const { instituteDetails } = useInstituteDetailsStore();
+
+    useEffect(() => {
+        console.log("data: ", data);
+        console.log("institute details: ", instituteDetails);
+    }, []);
+
     return <LayoutContainer></LayoutContainer>;
 }
