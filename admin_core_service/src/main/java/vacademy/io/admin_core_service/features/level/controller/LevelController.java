09ab540cdc0ac9c477a9e6d9f5149676cc51a1f3
Punit Punde <153176922+punitpunde@users.noreply.github.com>
2025-02-04 09:47:11 +0530

    Student portal (#38)
    * added student tracking apis
    
    * added level add,update,delete apis

---
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/level/controller/LevelController.java
@@ -0,0 +1,33 @@
+package vacademy.io.admin_core_service.features.level.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vacademy.io.admin_core_service.features.level.dto.AddLevelDTO;
+import vacademy.io.admin_core_service.features.level.service.LevelService;
+import vacademy.io.common.auth.model.CustomUserDetails;
+import vacademy.io.common.institute.dto.LevelDTO;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin-core-service/level/v1")
+@RequiredArgsConstructor
+public class LevelController {
+    private final LevelService levelService;
+
+    @PostMapping("/add-level")
+    public ResponseEntity<String>addLevel(@RequestBody AddLevelDTO addLevelDTO, @RequestAttribute("user")CustomUserDetails user,@RequestParam("packageId") String packageId,@RequestParam("sessionId")String sessionId) {
+        return ResponseEntity.ok(levelService.addLevel(addLevelDTO,packageId,sessionId,user));
+    }
+
+    @PutMapping("/update-level/{levelId}")
+    public ResponseEntity<LevelDTO>updateLevel(@RequestBody LevelDTO levelDTO, @RequestAttribute("user")CustomUserDetails user, @PathVariable("levelId") String levelId) {
+        return ResponseEntity.ok(levelService.updateLevel(levelId,levelDTO,user));
+    }
+
+    @DeleteMapping("/delete-level")
+    public ResponseEntity<String>deleteLevels(@RequestBody List<String> levelIds, @RequestAttribute("user")CustomUserDetails user) {
+        return ResponseEntity.ok(levelService.deleteLevels(levelIds,user));
+    }
+}
--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/level/service/LevelService.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/level/service/LevelService.java
@@ -1,17 +1,35 @@
 package vacademy.io.admin_core_service.features.level.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import vacademy.io.admin_core_service.features.level.dto.AddLevelDTO;
 import vacademy.io.admin_core_service.features.level.enums.LevelStatusEnum;
 import vacademy.io.admin_core_service.features.level.repository.LevelRepository;
+import vacademy.io.admin_core_service.features.packages.repository.PackageRepository;
+import vacademy.io.admin_core_service.features.packages.repository.PackageSessionRepository;
+import vacademy.io.admin_core_service.features.packages.service.PackageSessionService;
+import vacademy.io.admin_core_service.features.session.repository.SessionRepository;
+import vacademy.io.common.auth.model.CustomUserDetails;
 import vacademy.io.common.exceptions.VacademyException;
+import vacademy.io.common.institute.dto.LevelDTO;
 import vacademy.io.common.institute.entity.Level;
+import vacademy.io.common.institute.entity.PackageEntity;
+import vacademy.io.common.institute.entity.session.PackageSession;
+import vacademy.io.common.institute.entity.session.Session;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class LevelService {
     private final LevelRepository levelRepository;
+    private final PackageRepository packageRepository;
+    private final PackageSessionService packageSessionService;
+    private final SessionRepository sessionRepository;
 
     public Level createOrAddLevel(AddLevelDTO addLevelDTO) {
         Level level = null;
@@ -35,4 +53,47 @@ public class LevelService {
     public Level getLevelById(String levelId) {
         return levelRepository.findById(levelId).orElseThrow(() -> new VacademyException("Level not found"));
     }
+
+    @Transactional
+    public String addLevel(AddLevelDTO addLevelDTO, String packageId, String sessionId, CustomUserDetails user) {
+        Optional<PackageEntity>optionalPackageEntity = packageRepository.findById(packageId);
+        if (optionalPackageEntity.isEmpty()) {
+            throw new VacademyException("Package not found");
+        }
+        Optional<Session>optionalSession = sessionRepository.findById(sessionId);
+        if (optionalSession.isEmpty()) {
+            throw new VacademyException("Session not found");
+        }
+        Level level = createOrAddLevel(addLevelDTO);
+        packageSessionService.createPackageSession(level, optionalSession.get(), optionalPackageEntity.get(),new Date());
+        return level.getId();
+    }
+
+    public LevelDTO updateLevel(String levelId, LevelDTO levelDTO, CustomUserDetails user) {
+        Level level = getLevelById(levelId);
+
+        // Update level name only if the provided value is not null or empty.
+        if (levelDTO.getLevelName() != null && !levelDTO.getLevelName().trim().isEmpty()) {
+            level.setLevelName(levelDTO.getLevelName());
+        }
+
+        // Update duration only if the provided value is not null.
+        if (levelDTO.getDurationInDays() != null) {
+            level.setDurationInDays(levelDTO.getDurationInDays());
+        }
+        levelRepository.save(level);
+        return new LevelDTO(level);
+    }
+
+    public String deleteLevels(List<String> levelIds,CustomUserDetails user) {
+        List<Level> levels = levelRepository.findAllById(levelIds);
+
+        for (Level level : levels) {
+            level.setStatus(LevelStatusEnum.DELETED.name());
+        }
+
+        levelRepository.saveAll(levels); // Batch update
+        return "Levels deleted successfully";
+    }
+
 }
