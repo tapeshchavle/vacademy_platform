edbb39b98bc59b487caec73cf88c8f6255ddff06
Punit Punde <153176922+punitpunde@users.noreply.github.com>
2025-02-06 16:55:58 +0530

    Email auth (#47)
    * added hmac
    
    * Deleted NotificationService.java~
    
    * added api for student study library
    
    * added methods to send and verfiy otp
    
    * added notification server base url

---
+++ auth_service/src/main/java/vacademy/io/auth_service/feature/notification/service/NotificationService.java
@@ -0,0 +1,53 @@
+package vacademy.io.auth_service.feature.notification.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import vacademy.io.auth_service.feature.notification.constants.NotificationConstant;
+import vacademy.io.common.core.internal_api_wrapper.InternalClientUtils;
+import vacademy.io.common.notification.dto.EmailOTPRequest;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private InternalClientUtils internalClientUtils;
+
+    @Value("${spring.application.name}")
+    private String clientName;
+
+    @Value("${notification.server.baseurl}")
+    private String notificationServerBaseUrl;
+
+    public String sendOtp(EmailOTPRequest emailOTPRequest) {
+        // Removed the redundant 'clientName' parameter, we can use the injected clientName field here
+        ResponseEntity<String> response = internalClientUtils.makeHmacRequest(
+                clientName, // Directly use the injected 'clientName'
+                HttpMethod.POST.name(),
+                notificationServerBaseUrl,
+                NotificationConstant.SEND_EMAIL_OTP,
+                emailOTPRequest
+        );
+        return response.getBody();
+    }
+
+    public Boolean verifyOTP(EmailOTPRequest request) {
+
+        ResponseEntity<String> response = internalClientUtils.makeHmacRequest(clientName, HttpMethod.POST.name(), notificationServerBaseUrl, NotificationConstant.VERIFY_EMAIL_OTP, request);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Boolean isOtpValid = objectMapper.readValue(response.getBody(), new TypeReference<Boolean>() {
+            });
+
+            return isOtpValid;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
