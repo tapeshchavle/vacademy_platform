637c510afabeb407004d1dbcd9cc131ebbed14a6
Shubhashit <shubhashitjain@gmail.com>
2025-02-11 10:37:45 +0530

    "community service init api"

--- community_service/src/main/java/vacademy/io/community_service/controller/InitController.java
+++ community_service/src/main/java/vacademy/io/community_service/controller/InitController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/init")
+@RequestMapping("/community_service/init")
 public class InitController {
 
     @GetMapping
--- community_service/src/main/java/vacademy/io/community_service/enums/Level.java
+++ community_service/src/main/java/vacademy/io/community_service/enums/Level.java
@@ -1,10 +1,21 @@
 package vacademy.io.community_service.enums;
 
 public enum Level {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    SIXTH,
+    K12("K-12"),
+    HIGHER("Higher Education"),
+    CORPORATE("Corporate Training"),
+    COMMERCE("Commerce"),
+    ARTS("Arts"),
+    LAW("Law"),
+    MANAGEMENT("Management");
+
+    private final String displayName;
+
+    Level(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
