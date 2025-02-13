8cd80ecabe64354d40be044d1ed0351f576cce5e
Punit Punde <punitpunde@gmail.com>
2025-02-13 22:19:16 +0530

    fixed issue in get subject

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/subject/repository/SubjectRepository.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/subject/repository/SubjectRepository.java
@@ -10,8 +10,19 @@ import java.util.List;
 public interface SubjectRepository extends JpaRepository<Subject, String> {
 
 
-    @Query(nativeQuery = true, value = "SELECT DISTINCT s.* " + "FROM public.subject s " + "JOIN public.subject_session ss ON s.id = ss.subject_id " + "JOIN public.package_session ps ON ss.session_id = ps.id " + "JOIN public.package_institute pi ON ps.package_id = pi.package_id " + "WHERE pi.institute_id = :instituteId AND s.status = 'ACTIVE' ")
+    @Query(nativeQuery = true, value = """
+    SELECT DISTINCT ON (s.subject_name) s.*
+    FROM public.subject s
+    JOIN public.subject_session ss ON s.id = ss.subject_id
+    JOIN public.package_session ps ON ss.session_id = ps.id
+    JOIN public.package_institute pi ON ps.package_id = pi.package_id
+    WHERE pi.institute_id = :instituteId
+    AND s.status = 'ACTIVE'
+    AND ps.status != 'DELETED'
+    ORDER BY s.subject_name ASC, ss.subject_order ASC NULLS LAST
+""")
     List<Subject> findDistinctSubjectsByInstituteId(@Param("instituteId") String instituteId);
+
 
     @Query(value = "SELECT DISTINCT s.*, ss.subject_order " +
             "FROM subject s " +
