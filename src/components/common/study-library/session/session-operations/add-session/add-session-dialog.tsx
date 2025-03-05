ad88fa1591ab22235a0b825de113cef4ca452586
Shristi Gupta <shristiguptawork@gmail.com>
2025-03-05 21:04:27 +0530

    creating add session form

--- src/components/common/study-library/session/session-operations/add-session/add-session-dialog.tsx
+++ src/components/common/study-library/session/session-operations/add-session/add-session-dialog.tsx
@@ -2,12 +2,18 @@ import { MyButton } from "@/components/design-system/button";
 import { MyDialog } from "@/components/design-system/dialog";
 import { Plus } from "@phosphor-icons/react";
 import { useState } from "react";
+import { AddSessionDataType, AddSessionForm } from "./add-session-form";
 
 export const AddSessionDialog = () => {
     const [isAddSessionDiaogOpen, setIsAddSessionDiaogOpen] = useState(false);
+
     const handleOpenAddSessionDialog = () => {
         setIsAddSessionDiaogOpen(!isAddSessionDiaogOpen);
     };
+    const handleAddSession = (sessionData: AddSessionDataType) => {
+        console.log(sessionData);
+    };
+
     return (
         <MyDialog
             heading="Add session"
@@ -21,7 +27,7 @@ export const AddSessionDialog = () => {
             open={isAddSessionDiaogOpen}
             onOpenChange={handleOpenAddSessionDialog}
         >
-            adsf
+            <AddSessionForm onSubmit={handleAddSession} />
         </MyDialog>
     );
 };
--- src/components/common/study-library/session/session-operations/add-session/add-session-form.tsx
+++ src/components/common/study-library/session/session-operations/add-session/add-session-form.tsx
@@ -1,5 +1,108 @@
-import { Form } from "react-hook-form";
+import { MyInput } from "@/components/design-system/input";
+import { FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { useInstituteDetailsStore } from "@/stores/students/students-list/useInstituteDetailsStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { Form, useForm } from "react-hook-form";
+import { z } from "zod";
 
-export const AddSessionForm = () => {
-    return <Form></Form>;
+const formSchema = z.object({
+    id: z.string().nullable(),
+    session_name: z.string(),
+    status: z.string(),
+    start_date: z.string(),
+    new_session: z.boolean(),
+    levels: z.array(
+        z.object({
+            id: z.string().nullable(),
+            new_level: z.boolean(),
+            level_name: z.string(),
+            duration_in_days: z.number(),
+            thumbnail_file_id: z.string().nullable(),
+            package_id: z.string(),
+        }),
+    ),
+});
+export type LevelForSession = z.infer<typeof formSchema>["levels"][number];
+export type AddSessionDataType = z.infer<typeof formSchema>;
+
+export const AddSessionForm = ({
+    initialValues,
+    onSubmit,
+}: {
+    initialValues?: AddSessionDataType;
+    onSubmit: (sessionData: AddSessionDataType) => void;
+}) => {
+    const { instituteDetails, getPackageWiseLevels } = useInstituteDetailsStore();
+
+    const [packageWithLevels, setPackageWithLevels] = useState(getPackageWiseLevels());
+
+    useEffect(() => {
+        setPackageWithLevels(getPackageWiseLevels());
+        console.log(packageWithLevels);
+    }, [instituteDetails]);
+
+    const form = useForm<AddSessionDataType>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            id: initialValues?.id || null,
+            session_name: initialValues?.session_name || "",
+            status: initialValues?.status || "ACTIVE",
+            start_date: initialValues?.start_date || new Date().toISOString(),
+            new_session: initialValues ? false : true,
+            levels: initialValues?.levels || [], // Changed from levels to sessions
+        },
+    });
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={(e) => {
+                    form.handleSubmit(onSubmit)(e);
+                }}
+                className="flex max-h-[80vh] flex-col gap-8 overflow-y-auto p-2 text-neutral-600"
+            >
+                <FormField
+                    control={form.control}
+                    name="session_name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <MyInput
+                                    label="Session Name"
+                                    required={true}
+                                    inputType="text"
+                                    inputPlaceholder="Eg. 2024-2025"
+                                    className="w-full"
+                                    input={field.value}
+                                    onChangeFunction={(e) => field.onChange(e.target.value)}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="start_date"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <MyInput
+                                    label="Start Date"
+                                    required={true}
+                                    inputType="date"
+                                    inputPlaceholder="DD/MM/YYYY"
+                                    className="w-full"
+                                    input={field.value}
+                                    onChangeFunction={(e) => field.onChange(e.target.value)}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                {}
+            </form>
+        </Form>
+    );
 };
