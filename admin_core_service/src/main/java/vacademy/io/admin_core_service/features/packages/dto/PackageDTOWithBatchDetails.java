6188fbeb8b12fe1825ff4fe97bc60a97c902596a
Punit Punde <punitpunde@gmail.com>
2025-03-12 09:46:38 +0530

    added batch management apis

---
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/packages/dto/PackageDTOWithBatchDetails.java
@@ -0,0 +1,19 @@
+package vacademy.io.admin_core_service.features.packages.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vacademy.io.common.institute.dto.PackageDTO;
+
+import java.util.List;
+
+@Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@AllArgsConstructor
+@NoArgsConstructor
+public class PackageDTOWithBatchDetails {
+    private PackageDTO packageDTO;
+    private List<BatchProjection> batches;
+}
