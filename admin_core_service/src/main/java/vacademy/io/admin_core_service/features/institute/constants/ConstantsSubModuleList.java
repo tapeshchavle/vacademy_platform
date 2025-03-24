ff789bf82d092f630313fcb84ba728dd09480ca3
Piyush Raj <76215313+p-i-y-u-s-h-r-a-j@users.noreply.github.com>
2025-03-24 16:03:26 +0530

    fixed intitute-sub module-add and throw error on already existing user (#175)

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute/constants/ConstantsSubModuleList.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute/constants/ConstantsSubModuleList.java
@@ -9,28 +9,12 @@ public class ConstantsSubModuleList {
 
     // Static block to initialize the MAP
     static {
-        List<String> moduleOneSubModule = List.of("1");
-        List<String> moduleTwoSubModule = List.of("2");
+        List<String> moduleOneSubModule = List.of("1","3","5","6","7","8","9","10","11");
+        List<String> moduleTwoSubModule = List.of("2","4","12","13","15","16","17","18","19");
 
         SUB_MODULES_IDS.put("1", moduleOneSubModule);
-        SUB_MODULES_IDS.put("3", moduleOneSubModule);
-        SUB_MODULES_IDS.put("5", moduleOneSubModule);
-        SUB_MODULES_IDS.put("6", moduleOneSubModule);
-        SUB_MODULES_IDS.put("7", moduleOneSubModule);
-        SUB_MODULES_IDS.put("8", moduleOneSubModule);
-        SUB_MODULES_IDS.put("9", moduleOneSubModule);
-        SUB_MODULES_IDS.put("10", moduleOneSubModule);
-        SUB_MODULES_IDS.put("11", moduleOneSubModule);
 
         SUB_MODULES_IDS.put("2", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("4", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("12", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("13", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("15", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("16", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("17", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("18", moduleTwoSubModule);
-        SUB_MODULES_IDS.put("19", moduleTwoSubModule);
     }
 
     public static List<String> getSubModulesForModule(String moduleId) {
