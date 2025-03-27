42d222fb781acf658ea7cd41e4d8c9ae5a87f490
Punit Punde <punitpunde@gmail.com>
2025-03-27 13:30:56 +0530

    fixed issue in user role

--- auth_service/src/main/java/vacademy/io/auth_service/feature/user/util/RandomCredentialGenerator.java
+++ auth_service/src/main/java/vacademy/io/auth_service/feature/user/util/RandomCredentialGenerator.java
@@ -1,5 +1,7 @@
 package vacademy.io.auth_service.feature.user.util;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.security.SecureRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -13,13 +15,26 @@ public class RandomCredentialGenerator {
         return generateRandomString(6);
     }
 
-    public static String generateRandomUsername() {
-        return generateRandomString(6);
+    public static String generateRandomUsername(String fullName) {
+        return generateUsername(fullName);
     }
 
     private static String generateRandomString(int length) {
         return IntStream.range(0, length)
                 .mapToObj(i -> String.valueOf(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length()))))
                 .collect(Collectors.joining());
+    }
+
+    private static String generateUsername(String fullName) {
+        // Ensure full name has at least 4 characters, else pad with "X"
+        String namePart = fullName.replaceAll("\\s+", "").substring(0, Math.min(fullName.length(), 4)).toLowerCase();
+        if (namePart.length() < 4) {
+            namePart = String.format("%-4s", namePart).replace(' ', 'X');
+        }
+
+        // Generate 4 random digits
+        String randomDigits = RandomStringUtils.randomNumeric(4);
+
+        return namePart + randomDigits;
     }
 }
