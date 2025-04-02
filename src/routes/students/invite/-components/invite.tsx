e0aaa3908c61a734b8f8ceaa032628695da1c59f
Shristi Gupta <shristiguptawork@gmail.com>
2025-04-02 15:10:28 +0530

    implemented disable create button functionality

--- src/routes/students/invite/-components/invite.tsx
+++ src/routes/students/invite/-components/invite.tsx
@@ -17,6 +17,7 @@ import { useGetInviteList } from "../-services/get-invite-list";
 import { DashboardLoader } from "@/components/core/dashboard-loader";
 import createInviteLink from "../-utils/createInviteLink";
 import { useInviteFormContext } from "../-context/useInviteFormContext";
+import { useCourseManager } from "../-hooks/useCourseManager";
 
 export const Invite = () => {
     const [copySuccess, setCopySuccess] = useState<string | null>(null);
@@ -27,6 +28,8 @@ export const Invite = () => {
     const accessToken = getTokenFromCookie(TokenKey.accessToken);
     const tokenData = getTokenDecodedData(accessToken);
     const INSTITUTE_ID = tokenData && Object.keys(tokenData.authorities)[0];
+    const { hasValidPreSelectedCourseStructure, hasValidLearnerChoiceCourseStructure } =
+        useCourseManager();
 
     const { page, pageSize, handlePageChange } = usePaginationState({
         initialPage: 0,
@@ -68,7 +71,13 @@ export const Invite = () => {
             className="flex w-full items-center justify-end"
             onClick={() => formSubmitRef.current()}
         >
-            <MyButton>Create</MyButton>
+            <MyButton
+                disable={
+                    !hasValidLearnerChoiceCourseStructure() && !hasValidPreSelectedCourseStructure()
+                }
+            >
+                Create
+            </MyButton>
         </div>
     );
 
