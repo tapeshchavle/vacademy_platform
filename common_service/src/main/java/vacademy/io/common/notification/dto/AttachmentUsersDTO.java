e281380996e77aef8f6352b2429689629f6c8070
Punit Punde <punitpunde@gmail.com>
2025-04-09 17:31:09 +0530

    added attachment mail changes

--- common_service/src/main/java/vacademy/io/common/notification/dto/AttachmentUsersDTO.java
+++ common_service/src/main/java/vacademy/io/common/notification/dto/AttachmentUsersDTO.java
@@ -8,7 +8,14 @@ import java.util.Map;
 @Data
 public class AttachmentUsersDTO {
     private Map<String, String> placeholders;
-    private String attachment; // Base64 encoded string
+    private List<AttachmentDTO> attachments; // Base64 encoded string
     private String channelId;
     private String userId;
+
+    @Data
+    public static class AttachmentDTO {
+        private String attachmentName;
+        private String attachment;
+    }
+
 }
