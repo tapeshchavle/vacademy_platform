cc3cd91e73fdb95d33b539763b8648964a429007
Shristi Gupta <shristiguptawork@gmail.com>
2025-04-16 12:00:30 +0530

    created card for audio import

--- src/routes/ai-center/index.tsx
+++ src/routes/ai-center/index.tsx
@@ -1,6 +1,7 @@
 import { LayoutContainer } from "@/components/common/layout-container/layout-container";
 import { createFileRoute } from "@tanstack/react-router";
-import GenerateAIAssessmentComponent from "./-components/GenerateAssessment";
+import GenerateAIAssessmentComponent from "./-components/generate-assessment/GenerateAssessment";
+import { GenerateQuestionsFromAudio } from "./-components/generate-questions-from-audio/GenerateQuestionsFromAudio";
 
 export const Route = createFileRoute("/ai-center/")({
     component: () => (
@@ -12,8 +13,9 @@ export const Route = createFileRoute("/ai-center/")({
 
 function RouteComponent() {
     return (
-        <>
+        <div className="flex items-center justify-start gap-8">
             <GenerateAIAssessmentComponent />
-        </>
+            <GenerateQuestionsFromAudio />
+        </div>
     );
 }
