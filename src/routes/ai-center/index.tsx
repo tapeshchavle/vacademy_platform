642bfe88b6c51962d2614b740c74404a6d42f5a1
Shubhashit <shubhashitjain@gmail.com>
2025-04-16 12:16:05 +0530

    initial setup

--- src/routes/ai-center/index.tsx
+++ src/routes/ai-center/index.tsx
@@ -1,6 +1,7 @@
 import { LayoutContainer } from "@/components/common/layout-container/layout-container";
 import { createFileRoute } from "@tanstack/react-router";
 import GenerateAIAssessmentComponent from "./-components/GenerateAssessment";
+import GenerateAiQuestionPaperComponent from "./-components/-question-paper/GenerateQuestionPaper";
 
 export const Route = createFileRoute("/ai-center/")({
     component: () => (
@@ -12,8 +13,9 @@ export const Route = createFileRoute("/ai-center/")({
 
 function RouteComponent() {
     return (
-        <>
+        <div className="grid grid-cols-3 gap-4">
             <GenerateAIAssessmentComponent />
-        </>
+            <GenerateAiQuestionPaperComponent />
+        </div>
     );
 }
