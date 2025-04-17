dd529c7c881d45788d4d9b09818172ef24635254
Punit Punde <punitpunde@gmail.com>
2025-04-17 20:08:29 +0530

    addedevaulation tools apis

---
+++ media_service/src/main/java/vacademy/io/media_service/entity/TaskStatusEnum.java
@@ -0,0 +1,7 @@
+package vacademy.io.media_service.entity;
+
+public enum TaskStatusEnum {
+    PROCESSING,
+    COMPLETED,
+    FAILED
+}
---
+++ media_service/src/main/java/vacademy/io/media_service/evaluation_ai/dto/EvaluationResultFromDeepSeek.java
@@ -0,0 +1,42 @@
+package vacademy.io.media_service.evaluation_ai.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class EvaluationResultFromDeepSeek {
+    private List<EvaluationResult> evaluationResults;
+
+    @Data
+    public static class EvaluationResult {
+        private String userId;
+        private String name;
+        private String email;
+        private String contactNumber;
+        private double totalMarksObtained;
+        private double totalMarks;
+        private String overallVerdict; // pass/fail/absent/etc.
+        private List<SectionWiseResult> sectionWiseResults;
+    }
+
+    @Data
+    public static class SectionWiseResult {
+        private String sectionId;
+        private String sectionName;
+        private double marksObtained;
+        private double totalMarks;
+        private String verdict; // pass/fail/null
+        private List<QuestionWiseResult> questionWiseResults;
+    }
+
+    @Data
+    public static class QuestionWiseResult {
+        private String questionId;
+        private int questionOrder;
+        private double marksObtained;
+        private double totalMarks;
+        private String feedback;
+        private String verdict; // correct/partially correct/incorrect/null
+    }
+}
