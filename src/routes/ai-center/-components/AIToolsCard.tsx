abe6862875c13eb70601eabeb57e32ee63f089ce
Shiva Raj <shivaraj@Shivas-MacBook-Air.local>
2025-04-24 15:20:23 +0530

    Fixed some designs.

--- src/routes/ai-center/-components/AIToolsCard.tsx
+++ src/routes/ai-center/-components/AIToolsCard.tsx
@@ -10,14 +10,14 @@ export const AIToolsCard = ({ feature }: { feature: AIToolFeatureType }) => {
     return (
         <Card
             key={feature.key}
-            className="flex min-h-[332px] w-full cursor-pointer items-center justify-center gap-10 border-neutral-300 bg-neutral-50 p-10 text-neutral-600"
+            className="flex w-full cursor-pointer items-center justify-center gap-10 border-neutral-300 bg-neutral-50 p-8 text-neutral-600 sm:flex-wrap md:flex-nowrap"
             onClick={() => {
                 if (feature.route) {
                     navigate({ to: feature.route });
                 }
             }}
         >
-            <CardHeader className="flex h-full flex-col gap-3">
+            <CardHeader className="flex h-fit flex-col gap-3">
                 <CardTitle className="flex items-center gap-2 text-title font-semibold">
                     <StarFour size={30} weight="fill" className="text-primary-500" />{" "}
                     {feature.heading}
