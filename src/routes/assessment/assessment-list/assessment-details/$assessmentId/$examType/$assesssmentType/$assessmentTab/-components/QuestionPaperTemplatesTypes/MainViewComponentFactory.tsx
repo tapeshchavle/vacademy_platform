03a90e84d3697035909ae79359a462781e963ede
Shiva Raj <shivaraj@Shivas-MacBook-Air.local>
2025-05-24 15:53:08 +0530

    Fixed same thing in assessment.

--- src/routes/assessment/assessment-list/assessment-details/$assessmentId/$examType/$assesssmentType/$assessmentTab/-components/QuestionPaperTemplatesTypes/MainViewComponentFactory.tsx
+++ src/routes/assessment/assessment-list/assessment-details/$assessmentId/$examType/$assesssmentType/$assessmentTab/-components/QuestionPaperTemplatesTypes/MainViewComponentFactory.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { SingleCorrectQuestionPaperTemplateMainView } from "./MCQ(Single Correct)/SingleCorrectQuestionPaperTemplateMainView";
-import { MultipleCorrectQuestionPaperTemplateMainView } from "./MCQ(Multiple Correct)/MultipleCorrectQuestionPaperTemplateMainView";
-import { SectionQuestionPaperFormProps } from "../../-utils/assessment-question-paper";
-import { QuestionType } from "@/constants/dummy-data";
-import { NumericQuestionPaperTemplateMainView } from "./NumericType/NumericQuestionPaperTemplateMainView";
-import { ComprehensiveSingleCorrectQuestionPaperTemplateMainView } from "./Comprehensive MCQ(Single Correct)/ComprehensiveSingleCorrectQuestionPaperTemplateMainView";
-import { ComprehensiveMultipleCorrectQuestionPaperTemplateMainView } from "./Comprehensive MCQ(Multiple Correct)/ComprehensiveMultipleCorrectQuestionPaperTemplateMainView";
-import { ComprehensiveNumericQuestionPaperTemplateMainView } from "./ComprehensiveNumericType/ComprehensiveNumericQuestionPaperTemplateMainView";
-import { OneWordQuestionPaperTemplateMainView } from "./OneWordType/OneWordQuestionPaperTemplateMainView";
-import { LongAnswerQuestionPaperTemplateMainView } from "./LongAnswerType/LongAnswerQuestionPaperTemplateMainView";
-import { TrueFalseQuestionPaperTemplateMainView } from "./TrueFalse/TrueFalseQuestionPaperTemplateMainView";
+import React from 'react';
+import { SingleCorrectQuestionPaperTemplateMainView } from './MCQ(Single Correct)/SingleCorrectQuestionPaperTemplateMainView';
+import { MultipleCorrectQuestionPaperTemplateMainView } from './MCQ(Multiple Correct)/MultipleCorrectQuestionPaperTemplateMainView';
+import { SectionQuestionPaperFormProps } from '../../-utils/assessment-question-paper';
+import { QuestionType } from '@/constants/dummy-data';
+import { NumericQuestionPaperTemplateMainView } from './NumericType/NumericQuestionPaperTemplateMainView';
+import { ComprehensiveSingleCorrectQuestionPaperTemplateMainView } from './Comprehensive MCQ(Single Correct)/ComprehensiveSingleCorrectQuestionPaperTemplateMainView';
+import { ComprehensiveMultipleCorrectQuestionPaperTemplateMainView } from './Comprehensive MCQ(Multiple Correct)/ComprehensiveMultipleCorrectQuestionPaperTemplateMainView';
+import { ComprehensiveNumericQuestionPaperTemplateMainView } from './ComprehensiveNumericType/ComprehensiveNumericQuestionPaperTemplateMainView';
+import { OneWordQuestionPaperTemplateMainView } from './OneWordType/OneWordQuestionPaperTemplateMainView';
+import { LongAnswerQuestionPaperTemplateMainView } from './LongAnswerType/LongAnswerQuestionPaperTemplateMainView';
+import { TrueFalseQuestionPaperTemplateMainView } from './TrueFalse/TrueFalseQuestionPaperTemplateMainView';
 
 type MainViewComponentType = QuestionType;
 
@@ -31,6 +31,9 @@ export const MainViewComponentFactory = (params: {
     type: MainViewComponentType;
     props: SectionQuestionPaperFormProps;
 }) => {
-    const Component = MainViewComponentsMap[params.type];
-    return <Component {...params.props} />;
+    if (params.type) {
+        const Component = MainViewComponentsMap[params.type];
+        return <Component {...params.props} />;
+    }
+    return;
 };
