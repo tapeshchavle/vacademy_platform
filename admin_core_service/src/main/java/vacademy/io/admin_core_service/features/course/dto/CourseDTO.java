1c1575e1ab1c2bcd374f082dd8ecfa779adae150
punitpunde <punitpunde@gmail.com>
2025-06-02 13:20:42 +0530

    added course detail

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/course/dto/CourseDTO.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/course/dto/CourseDTO.java
@@ -2,6 +2,8 @@ package vacademy.io.admin_core_service.features.course.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Column;
+import jakarta.persistence.Lob;
 import lombok.Getter;
 import lombok.Setter;
 import vacademy.io.common.institute.entity.PackageEntity;
@@ -14,11 +16,31 @@ public class CourseDTO {
     private String packageName;
     private String thumbnailFileId;
     private String status;
+    private Boolean isCoursePublishedToCatalaouge;
+    private String coursePreviewImageMediaId;
+    private String courseBannerMediaId;
+    private String courseMediaId;
+    private String whyLearn;
+    private String whoShouldLearn;
+    private String aboutTheCourse;
+    private String tags;
+    private Integer courseDepth;
+    private String courseHtmlDescription;
 
     public CourseDTO(PackageEntity packageEntity) {
         this.id = packageEntity.getId();
         this.packageName = packageEntity.getPackageName();
         this.thumbnailFileId = packageEntity.getThumbnailFileId();
         this.status = packageEntity.getStatus();
+        this.courseDepth = packageEntity.getCourseDepth();
+        this.isCoursePublishedToCatalaouge = packageEntity.getIsCoursePublishedToCatalaouge();
+        this.coursePreviewImageMediaId = packageEntity.getCoursePreviewImageMediaId();
+        this.courseBannerMediaId = packageEntity.getCourseBannerMediaId();
+        this.courseMediaId = packageEntity.getCourseMediaId();
+        this.whyLearn = packageEntity.getWhyLearn();
+        this.whoShouldLearn = packageEntity.getWhoShouldLearn();
+        this.aboutTheCourse = packageEntity.getAboutTheCourse();
+        this.tags = packageEntity.getTags();
+        this.courseHtmlDescription = packageEntity.getCourseHtmlDescription();
     }
 }
