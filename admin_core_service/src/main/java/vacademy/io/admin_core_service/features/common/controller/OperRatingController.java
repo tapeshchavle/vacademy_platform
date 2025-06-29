610324ddfbc9959d276d33cd164ed22800f8f2d1
Punit Punde <punitpunde@gmail.com>
2025-06-29 19:01:12 +0530

    added public rating api

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/common/controller/OperRatingController.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/common/controller/OperRatingController.java
@@ -18,45 +18,30 @@ public class OperRatingController {
     @Autowired
     private RatingService ratingService;
 
-    @PostMapping
-    public ResponseEntity<String> addRating(@RequestBody RatingDTO ratingDTO, @RequestAttribute("user") CustomUserDetails user) {
-        String response = ratingService.addRating(ratingDTO);
-        return ResponseEntity.ok(response);
-    }
-
-    @PutMapping
-    public ResponseEntity<String>updateRating(@RequestBody RatingDTO ratingDTO, @RequestAttribute("user") CustomUserDetails user) {
-        String response = ratingService.updateRating(ratingDTO);
-        return ResponseEntity.ok(response);
-    }
 
     @PostMapping("/get-package-ratings")
     public ResponseEntity<Page<RatingDetailDTO>> getPackageRatings(@RequestBody RatingFilterDTO ratingFilterDTO,
                                                                    @RequestParam(defaultValue = PageConstants.DEFAULT_PAGE_NUMBER) int pageNo,
-                                                                   @RequestParam(defaultValue = PageConstants.DEFAULT_PAGE_SIZE) int pageSize,
-                                                                   @RequestAttribute("user") CustomUserDetails user) {
+                                                                   @RequestParam(defaultValue = PageConstants.DEFAULT_PAGE_SIZE) int pageSize) {
         return ResponseEntity.ok(ratingService.getRatingsForPackage(ratingFilterDTO,pageNo,pageSize));
     }
 
     @PostMapping("/get-source-ratings")
     public ResponseEntity<Page<RatingDetailDTO>> getSourceRatings(@RequestBody RatingFilterDTO ratingFilterDTO,
                                                                   @RequestParam(defaultValue = PageConstants.DEFAULT_PAGE_NUMBER) int pageNo,
-                                                                  @RequestParam(defaultValue = PageConstants.DEFAULT_PAGE_SIZE) int pageSize,
-                                                                  @RequestAttribute("user") CustomUserDetails user) {
-        return ResponseEntity.ok(ratingService.getRatingsForSource(ratingFilterDTO,pageNo,pageSize, user));
+                                                                  @RequestParam(defaultValue = PageConstants.DEFAULT_PAGE_SIZE) int pageSize) {
+        return ResponseEntity.ok(ratingService.getRatingsForSource(ratingFilterDTO,pageNo,pageSize, null));
     }
 
     @GetMapping("/summary")
     public ResponseEntity<RatingSummaryProjection> getSourceRatingsSummary(String sourceType,
-                                                                           String sourceId,
-                                                                           @RequestAttribute("user") CustomUserDetails user) {
+                                                                           String sourceId) {
         return ResponseEntity.ok(ratingService.getRatingSummaryProjectionForSource(sourceType, sourceId));
     }
 
     @GetMapping("/package-summary")
     public ResponseEntity<RatingSummaryProjection> getPackageRatingsSummary(String sourceType,
-                                                                            String sourceId,
-                                                                            @RequestAttribute("user") CustomUserDetails user) {
+                                                                            String sourceId) {
         return ResponseEntity.ok(ratingService.getRatingSummaryProjectionForPackage(sourceType, sourceId));
     }
 }
