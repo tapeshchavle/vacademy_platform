0bf578704556be8737a143ae5c6c8804dd2026e3
unknown <shreyashjain1996@gmail.com>
2025-07-18 13:45:11 +0530

    updated notification service

--- notification_service/src/main/java/vacademy/io/notification_service/features/firebase_notifications/repository/FcmTokenRepository.java
+++ notification_service/src/main/java/vacademy/io/notification_service/features/firebase_notifications/repository/FcmTokenRepository.java
@@ -3,13 +3,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import vacademy.io.notification_service.features.firebase_notifications.entity.FcmToken;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+public interface FcmTokenRepository extends JpaRepository<FcmToken, String> {
 
     List<FcmToken> findByUserIdAndIsActiveTrue(String userId);
 
@@ -18,12 +20,14 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
     Optional<FcmToken> findByUserIdAndDeviceIdAndIsActiveTrue(String userId, String deviceId);
 
     @Modifying
+    @Transactional
     @Query("UPDATE FcmToken f SET f.isActive = false WHERE f.userId = :userId AND f.deviceId = :deviceId")
-    void deactivateTokenByUserIdAndDeviceId(String userId, String deviceId);
+    void deactivateTokenByUserIdAndDeviceId(@Param("userId") String userId, @Param("deviceId") String deviceId);
 
     @Modifying
+    @Transactional
     @Query("UPDATE FcmToken f SET f.isActive = false WHERE f.token = :token")
-    void deactivateTokenByToken(String token);
+    void deactivateTokenByToken(@Param("token") String token);
 
     List<FcmToken> findByIsActiveTrue();
 }
