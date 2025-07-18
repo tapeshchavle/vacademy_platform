b9ec4731e4e82e460f789f1501e1569216291577
Shiva Raj <shivaraj@Shivas-MacBook-Air.local>
2025-07-18 10:41:47 +0530

    Added domain API

--- src/routes/courses/-services/courses-services.ts
+++ src/routes/courses/-services/courses-services.ts
@@ -1,4 +1,6 @@
 import { getInstituteId } from "@/constants/helper";
+import { GET_SUBDOMAIN_OR_INSTITUTEID } from "@/constants/urls";
+import axios from "axios";
 
 export const fetchCourseDetails = async (courseId: string) => {
     const instituteId = getInstituteId();
@@ -10,4 +12,31 @@ export const fetchCourseDetails = async (courseId: string) => {
         throw new Error("Failed to fetch course details");
     }
     return response.json();
+};
+
+export const getInstituteIdBySubdomain = async ({
+    subdomain,
+}: {
+    subdomain: string;
+}) => {
+    const response = await axios({
+        method: "GET",
+        url: GET_SUBDOMAIN_OR_INSTITUTEID,
+        params: {
+            subdomain,
+            instituteId: "",
+        },
+    });
+    return response?.data;
+};
+export const handleGetInstituteIdBySubdomain = ({
+    subdomain,
+}: {
+    subdomain: string;
+}) => {
+    return {
+        queryKey: ["GET_INSTITUTEID_OR_SUBDOMAIN", subdomain],
+        queryFn: () => getInstituteIdBySubdomain({ subdomain }),
+        staleTime: 60 * 60 * 1000,
+    };
 };
--- src/routes/courses/index.tsx
+++ src/routes/courses/index.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import CourseCatalougePage from "./-component/CourseCatalougePage";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { handleGetInstituteIdBySubdomain } from "./-services/courses-services";
+import { useEffect } from "react";
+import { useNavigate, useSearch } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/courses/")({
     component: CoursesContainerComponent,
@@ -12,7 +16,39 @@ export const Route = createFileRoute("/courses/")({
     },
 });
 
+function getSubdomain(hostname: string) {
+    // e.g. learner.vacademy.io => learner
+    const parts = hostname.split(".");
+    if (parts.length >= 3) {
+        return parts[0];
+    }
+    return null;
+}
+
 function CoursesContainerComponent() {
+    const navigate = useNavigate();
+    const search = useSearch({ from: "/courses/" });
+    const subdomain = getSubdomain(window.location.hostname);
+    console.log(window.location);
+    const { data: apiResult, isLoading } = useSuspenseQuery(
+        handleGetInstituteIdBySubdomain({
+            subdomain: subdomain || "",
+        })
+    );
+    useEffect(() => {
+        if (
+            !isLoading &&
+            apiResult &&
+            apiResult.id &&
+            search.instituteId !== apiResult.id
+        ) {
+            navigate({
+                to: "/courses",
+                search: { ...search, instituteId: apiResult.id },
+                replace: true,
+            });
+        }
+    }, [apiResult, isLoading, search, navigate]);
     return (
         <div className="min-h-screen bg-white">
             <CourseCatalougePage />
