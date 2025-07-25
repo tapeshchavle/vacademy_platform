5972a8a999f6964ea323d75fa761841bec67db5e
Gulshan Punde <gulshanpunde1@gmail.com>
2025-07-25 10:34:02 +0530

    Course name, session name, level name, subject name, module name, chapter name in Title Case

--- src/routes/study-library/doubt-management/-components/doubt-table/batch-cell.tsx
+++ src/routes/study-library/doubt-management/-components/doubt-table/batch-cell.tsx
@@ -1,15 +1,16 @@
 import { useInstituteDetailsStore } from '@/stores/students/students-list/useInstituteDetailsStore';
+import { convertCapitalToTitleCase } from '@/lib/utils';
 
 export const BatchCell = ({ batch_id }: { batch_id: string }) => {
     const { instituteDetails } = useInstituteDetailsStore();
     const batch = instituteDetails?.batches_for_sessions?.find((batch) => batch.id == batch_id);
 
     const batchName = batch
-        ? batch.level.level_name +
+        ? convertCapitalToTitleCase(batch.level.level_name) +
           ' ' +
-          batch.package_dto.package_name +
+          convertCapitalToTitleCase(batch.package_dto.package_name) +
           ' ' +
-          batch.session.session_name
+          convertCapitalToTitleCase(batch.session.session_name)
         : '';
 
     return (
