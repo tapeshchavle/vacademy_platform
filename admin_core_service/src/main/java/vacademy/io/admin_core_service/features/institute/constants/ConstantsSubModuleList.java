2889bf2f0e8f2662ca85f973de2dc92c1c128db7
Gulshan Punde <gulshanpunde1@gmail.com>
2025-07-31 18:52:15 +0530

    added atendace sub module

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute/constants/ConstantsSubModuleList.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute/constants/ConstantsSubModuleList.java
@@ -10,7 +10,7 @@ public class ConstantsSubModuleList {
     // Static block to initialize the MAP
     static {
         List<String> assessModuleSubModule = List.of("1", "3", "5", "6", "7", "8", "9", "10", "11","20", "21", "24", "25", "26", "22", "23", "27", "28", "29");
-        List<String> lmsModuleSubModule = List.of("2", "4", "12", "13", "15", "16", "17", "18", "19","30", "31", "32", "33", "34", "35", "36", "37");
+        List<String> lmsModuleSubModule = List.of("2", "4", "12", "13", "15", "16", "17", "18", "19","30", "31", "32", "33", "34", "35", "36", "37","44");
         List<String> voltModuleSubModule = List.of("38","39");
         List<String> vsmartModuleSubModule = List.of("41","42","43");
 
