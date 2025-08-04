a352f6d1ae79d21d281d50802830521061dde864
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2025-08-04 20:39:35 +0530

    added fix to get by email

--- auth_service/src/main/java/vacademy/io/auth_service/feature/user/controller/OpenUserController.java
+++ auth_service/src/main/java/vacademy/io/auth_service/feature/user/controller/OpenUserController.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import vacademy.io.auth_service.feature.user.service.UserOperationService;
 import vacademy.io.common.auth.dto.UserDTO;
-import vacademy.io.common.auth.entity.User;
 
 import java.util.Objects;
 
@@ -20,11 +20,11 @@ public class OpenUserController {
     private UserOperationService userOperationService;
 
     @PostMapping("/by-email")
-    public ResponseEntity<UserDTO>findUserByEmail(String emailid){
-        UserDTO user = userOperationService.findUserByEmail(emailid);
+    public ResponseEntity<UserDTO> findUserByEmail(@RequestParam String emailId){
+        UserDTO user = userOperationService.findUserByEmail(emailId);
         if (Objects.isNull(user)){
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(userOperationService.findUserByEmail(emailid));
+        return ResponseEntity.ok(user);
     }
 }
