e956ce940aacd666e2cfe19882263378664c3f37
Shiva Raj <abshivaraj555@gmail.com>
2025-08-21 18:11:59 +0530

    Added teacher url from backend.

--- src/components/common/layout-container/top-navbar.tsx/navbar.tsx
+++ src/components/common/layout-container/top-navbar.tsx/navbar.tsx
@@ -21,7 +21,7 @@ import { DashboardLoader } from "@/components/core/dashboard-loader";
 import { getTokenFromCookie } from "@/lib/auth/sessionUtility";
 import { TokenKey } from "@/constants/auth/tokens";
 import { SystemAlertsBar } from "@/components/announcements";
-import { BASE_URL_TEACHER_DASHBOARD } from "@/constants/urls";
+import { handleGetPublicInstituteDetails } from "../services/navbar-services";
 
 interface UserRole {
     id: string;
@@ -32,6 +32,9 @@ interface UserRole {
 }
 
 export function Navbar() {
+    const { data: instituteDetails } = useSuspenseQuery(
+        handleGetPublicInstituteDetails()
+    );
     const { data: userRoleDetails, isLoading } = useSuspenseQuery(
         handleFetchUserRoleDetails()
     );
@@ -55,7 +58,7 @@ export function Navbar() {
     const handleNavigateToAdmin = () => {
         const accessToken = getTokenFromCookie(TokenKey.accessToken);
         const refreshToken = getTokenFromCookie(TokenKey.refreshToken);
-        window.location.href = `${BASE_URL_TEACHER_DASHBOARD.replace(/\/$/, "")}/auth-transfer?accessToken=${accessToken}&refreshToken=${refreshToken}`;
+        window.location.href = `https://${instituteDetails.teacher_portal_base_url}/auth-transfer?accessToken=${accessToken}&refreshToken=${refreshToken}`;
     };
 
     async function fetch() {
