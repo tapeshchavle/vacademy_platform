8d025fea627406ff57b0820cec886a8299b9b093
Tapesh Chavle <tapeshchawle@gmail.com>
2025-08-25 10:28:49 +0530

    fixed email notifications

--- notification_service/src/main/java/vacademy/io/notification_service/service/EmailService.java
+++ notification_service/src/main/java/vacademy/io/notification_service/service/EmailService.java
@@ -61,7 +61,6 @@ public class EmailService {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
