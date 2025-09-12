b9264a3e5ef304c27a6b8066449cf04dfc40c95c
Punit P <punitpunde@gmail.com>
2025-09-12 13:41:14 +0530

    Update StudentSessionInstituteGroupMappingRepository.java

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute_learner/repository/StudentSessionInstituteGroupMappingRepository.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute_learner/repository/StudentSessionInstituteGroupMappingRepository.java
@@ -1,3 +1,14 @@
+package vacademy.io.admin_core_service.features.institute_learner.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import vacademy.io.admin_core_service.features.institute_learner.entity.StudentSessionInstituteGroupMapping;
+
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudentSessionInstituteGroupMappingRepository
     extends JpaRepository<StudentSessionInstituteGroupMapping, String> {
