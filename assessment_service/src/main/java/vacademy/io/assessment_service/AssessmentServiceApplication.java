483a06f6ebdf7f016f39884202336f0ebf5a6786
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2025-09-18 13:41:26 +0530

    added trigger for assess and comm

--- assessment_service/src/main/java/vacademy/io/assessment_service/AssessmentServiceApplication.java
+++ assessment_service/src/main/java/vacademy/io/assessment_service/AssessmentServiceApplication.java
@@ -18,6 +18,7 @@ public class AssessmentServiceApplication {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(AssessmentServiceApplication.class, args);
+        
     }
 
     @PostConstruct
