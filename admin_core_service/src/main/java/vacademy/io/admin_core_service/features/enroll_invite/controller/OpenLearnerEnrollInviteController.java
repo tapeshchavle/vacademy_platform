792c5e20f387871a58a6560f6719009d5dd1d2ea
Punit Punde <punitpunde@gmail.com>
2025-10-03 17:37:06 +0530

    added public enroll invite api

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/enroll_invite/controller/OpenLearnerEnrollInviteController.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/enroll_invite/controller/OpenLearnerEnrollInviteController.java
@@ -3,6 +3,7 @@ package vacademy.io.admin_core_service.features.enroll_invite.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vacademy.io.admin_core_service.features.enroll_invite.dto.EnrollInviteDTO;
@@ -17,9 +18,16 @@ public class OpenLearnerEnrollInviteController {
     @Autowired
     private LearnerEnrollInviteService learnerEnrollInviteService;
 
+    @Autowired
+    private EnrollInviteService enrollInviteService;
 
     @GetMapping
     public ResponseEntity<EnrollInviteDTO>getEnrollInvite(String instituteId,String inviteCode) {
         return ResponseEntity.ok(learnerEnrollInviteService.getEnrollInvite(instituteId, inviteCode));
+    }
+
+    @GetMapping("/{instituteId}/{enrollInviteId}")
+    public ResponseEntity<EnrollInviteDTO>getEnrollInviteById(@PathVariable("instituteId") String instituteId, @PathVariable("enrollInviteId") String enrollInviteId) {
+        return ResponseEntity.ok(enrollInviteService.findByEnrollInviteId(enrollInviteId,instituteId));
     }
 }
