862db93208657bee4581983fc36bf4114f1ebd8a
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2025-10-08 11:45:04 +0530

    added cors

--- community_service/src/main/java/vacademy/io/community_service/config/CorsConfig.java
+++ community_service/src/main/java/vacademy/io/community_service/config/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowCredentials(false)
                 .allowedOrigins("*") // Allow requests from any origin
                 .allowedMethods("*")
                 .allowCredentials(true) // Allow any HTTP method (GET, POST, etc.)
