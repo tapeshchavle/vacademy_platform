00a9e58da322c2ee8fbb8316ff4447238c8a2c8d
Punit Punde <punitpunde@gmail.com>
2025-10-18 10:59:29 +0530

    added user purchase api

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/user_subscription/service/UserPlanService.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/user_subscription/service/UserPlanService.java
@@ -3,6 +3,7 @@ package vacademy.io.admin_core_service.features.user_subscription.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
