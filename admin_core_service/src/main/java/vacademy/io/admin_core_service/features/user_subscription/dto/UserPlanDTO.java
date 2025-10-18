fba16a5763e44fb86a2441bb9d889a265e4d944e
Punit Punde <punitpunde@gmail.com>
2025-10-18 10:53:24 +0530

    added api to get all last user plans of use

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/user_subscription/dto/UserPlanDTO.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/user_subscription/dto/UserPlanDTO.java
@@ -1,9 +1,12 @@
 package vacademy.io.admin_core_service.features.user_subscription.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import vacademy.io.admin_core_service.features.enroll_invite.dto.EnrollInviteDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserPlanDTO {
 
     private String id;
@@ -37,4 +41,10 @@ public class UserPlanDTO {
     private LocalDateTime updatedAt;
 
     private List<PaymentLogDTO>paymentLogs;
+
+    private EnrollInviteDTO enrollInvite;
+
+    private PaymentOptionDTO paymentOption;
+
+    private PaymentPlanDTO paymentPlanDTO;
 }
