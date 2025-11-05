f3f9ccc9512b833cfde5955d1d6bf5dc392930cd
Tapesh Chavle <tapeshchawle@gmail.com>
2025-11-05 14:14:50 +0530

    fixed json ignore

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute/dto/settings/SettingDto.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/institute/dto/settings/SettingDto.java
@@ -1,11 +1,13 @@
 package vacademy.io.admin_core_service.features.institute.dto.settings;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SettingDto {
     private String key;
     private String name;
