bd6e4931b74b3f6d231cd232e28f3038f6c3b33c
Tapesh Chavle <tapeshchawle@gmail.com>
2025-11-14 17:10:17 +0530

    fixed minor-changes

--- common_service/src/main/java/vacademy/io/common/auth/dto/UserDTO.java
+++ common_service/src/main/java/vacademy/io/common/auth/dto/UserDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import vacademy.io.common.auth.entity.User;
@@ -14,6 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
