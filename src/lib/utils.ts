222b70011a3aa1242655bb91a97cc8aa6d381971
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2025-11-17 14:52:05 +0530

    correct issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,18 +12,7 @@ export function isNullOrEmptyOrUndefined<T>(
 }
 
 export function convertCapitalToTitleCase(str: string) {
-    // Split the text into words
-    const words = str?.split(' ');
-
-    // Capitalize the first letter of each word and convert the rest to lowercase
-    const titleCaseWords = words?.map(
-        (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-    );
-
-    // Join the words back into a string
-    const titleCaseText = titleCaseWords?.join(' ');
-
-    return titleCaseText;
+    return str;
 }
 
 export function parseHtmlToString(html: string) {
