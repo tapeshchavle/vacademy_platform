4313b6e7fec4f4ce2b51bc8f8dde92c3e33858db
Tapesh Chavle <tapeshchawle@gmail.com>
2025-12-02 15:46:50 +0530

    fixed field type

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/common/enums/FieldTypeEnum.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/common/enums/FieldTypeEnum.java
@@ -3,11 +3,6 @@ package vacademy.io.admin_core_service.features.common.enums;
 public enum FieldTypeEnum {
     TEXT,
     DROPDOWN,
-    NUMBER,
-    DATE,
-    CHECKBOX,
-    TEXTAREA,
     EMAIL,
-    PHONE,
     URL
 }
