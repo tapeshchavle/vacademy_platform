c1e2aef6a3847eb6d496077def54e4da7b14bed5
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2025-12-14 00:38:06 +0530

    added instructor co-pilot apis

---
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/instructor_copilot/dto/CreateInstructorCopilotLogRequest.java
@@ -0,0 +1,15 @@
+package vacademy.io.admin_core_service.features.instructor_copilot.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CreateInstructorCopilotLogRequest {
+    private String transcriptJson;
+}
---
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/instructor_copilot/dto/InstructorCopilotLogDTO.java
@@ -0,0 +1,52 @@
+package vacademy.io.admin_core_service.features.instructor_copilot.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vacademy.io.admin_core_service.features.instructor_copilot.entity.InstructorCopilotLog;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class InstructorCopilotLogDTO {
+    private String id;
+    private String createdByUserId;
+    private String instituteId;
+    private String title;
+    private String thumbnailFileId;
+    private String transcriptJson;
+    private String flashnotesJson;
+    private String summary;
+    private String questionJson;
+    private String flashcardJson;
+    private String slidesJson;
+    private String videoJson;
+    private String status;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public InstructorCopilotLogDTO(InstructorCopilotLog log) {
+        this.id = log.getId();
+        this.createdByUserId = log.getCreatedByUserId();
+        this.instituteId = log.getInstituteId();
+        this.title = log.getTitle();
+        this.thumbnailFileId = log.getThumbnailFileId();
+        this.transcriptJson = log.getTranscriptJson();
+        this.flashnotesJson = log.getFlashnotesJson();
+        this.summary = log.getSummary();
+        this.questionJson = log.getQuestionJson();
+        this.flashcardJson = log.getFlashcardJson();
+        this.slidesJson = log.getSlidesJson();
+        this.videoJson = log.getVideoJson();
+        this.status = log.getStatus();
+        this.createdAt = log.getCreatedAt();
+        this.updatedAt = log.getUpdatedAt();
+    }
+}
