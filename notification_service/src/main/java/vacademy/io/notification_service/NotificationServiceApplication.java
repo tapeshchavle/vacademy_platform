b976eacb1673f616737ad47a889cf3934d786abf
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2025-12-19 20:33:27 +0530

    added auth service analytics api caching

--- notification_service/src/main/java/vacademy/io/notification_service/NotificationServiceApplication.java
+++ notification_service/src/main/java/vacademy/io/notification_service/NotificationServiceApplication.java
@@ -15,5 +15,6 @@ public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
         
+        
     }
 }
