fe76c4c47eadcfca5de93e39c8fde54606974a4a
Shubhamydv-07 <yshubh140704@gmail.com>
2026-01-06 15:36:25 +0530

    fix:reverted JsonProperty from PaymentInitiationRequestDTO

--- common_service/src/main/java/vacademy/io/common/payment/dto/PaymentInitiationRequestDTO.java
+++ common_service/src/main/java/vacademy/io/common/payment/dto/PaymentInitiationRequestDTO.java
@@ -1,6 +1,5 @@
 package vacademy.io.common.payment.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
@@ -12,23 +11,12 @@ public class PaymentInitiationRequestDTO {
     private Double amount;
     private String currency;
     private String description;
-
-    @JsonProperty("is_include_pending_items")
     private boolean isIncludePendingItems;
-
-    @JsonProperty("charge_automatically")
     private boolean chargeAutomatically;
-
-    @JsonProperty("order_id")
     private String orderId;
-
-    @JsonProperty("institute_id")
     private String instituteId;
-
     private String email;
     private String vendor;
-
-    @JsonProperty("vendor_id")
     private String vendorId;
     private StripeRequestDTO stripeRequest;
     private RazorpayRequestDTO razorpayRequest;
