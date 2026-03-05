830e87b0b21a8bbfa7cbed96f23ac3c343fbf6e5
samar Krishna <amitkumar231677@gmail.com>
2026-03-05 12:38:49 +0530

    fix: fix delete api not working becuase the api that was being hit did not exist

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -433,7 +433,7 @@ export const LIVE_SESSION_ALL_ATTENDANCE = `${BASE_URL}/admin-core-service/live-
 export const REFERRAL_API_BASE = `${BASE_URL}/admin-core-service/v1/referral-option`;
 export const REFERRAL_UPDATE = (referralOptionId: string) =>
     `${BASE_URL}/admin-core-service/v1/referral-option/${referralOptionId}`;
-export const REFERRAL_DELETE = `${BASE_URL}/admin-core-service/v1/referral-option/referral-option`;
+export const REFERRAL_DELETE = `${BASE_URL}/admin-core-service/v1/referral-option`;
 
 export const GET_INSITITUTE_SETTINGS = `${BASE_URL}/admin-core-service/institute/setting/v1/get`;
 export const UPDATE_CUSTOM_FIELD_SETTINGS = `${BASE_URL}/admin-core-service/institute/v1/custom-field/create-or-update`;
