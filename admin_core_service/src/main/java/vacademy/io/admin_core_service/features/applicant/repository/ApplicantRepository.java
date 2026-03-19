c98316b1d10023218e16ed65fa7120c8ed3076b0
samar Krishna <amitkumar231677@gmail.com>
2026-03-19 12:45:18 +0530

    fix: add query for filtering with phone in search paramter in applicatRepository while fetching list of applicant

--- admin_core_service/src/main/java/vacademy/io/admin_core_service/features/applicant/repository/ApplicantRepository.java
+++ admin_core_service/src/main/java/vacademy/io/admin_core_service/features/applicant/repository/ApplicantRepository.java
@@ -48,7 +48,9 @@ public interface ApplicantRepository extends JpaRepository<Applicant, UUID> {
                         "LOWER(a.trackingId) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
                         "LOWER(CAST(a.id AS string)) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
                         "LOWER(a.applicationStageId) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
-                        "a.id IN (SELECT CAST(ar.applicantId AS uuid) FROM AudienceResponse ar WHERE ar.studentUserId IN (SELECT s.userId FROM Student s WHERE LOWER(s.fullName) LIKE LOWER(CONCAT('%', :search, '%')))))")
+                        "a.id IN (SELECT CAST(ar.applicantId AS uuid) FROM AudienceResponse ar WHERE ar.studentUserId IN (SELECT s.userId FROM Student s WHERE LOWER(s.fullName) LIKE LOWER(CONCAT('%', :search, '%')))) OR "
+                        +
+                        "a.id IN (SELECT CAST(ar.applicantId AS uuid) FROM AudienceResponse ar WHERE LOWER(ar.parentMobile) LIKE LOWER(CONCAT('%', :search, '%'))))")
         Page<Applicant> findApplicantsWithEnhancedFilters(
                         @Param("instituteId") String instituteId,
                         @Param("source") String source,
