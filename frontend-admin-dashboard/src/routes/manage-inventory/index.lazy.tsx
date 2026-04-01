9dc9924055e304bf37fbdc2e4eb168f591a00429
Samar Krishna <samarkrishna231@gmail.com>
2026-04-01 13:14:45 +0530

    fix: wrap ManageInventoryPage with layoutContainer

--- frontend-admin-dashboard/src/routes/manage-inventory/index.lazy.tsx
+++ frontend-admin-dashboard/src/routes/manage-inventory/index.lazy.tsx
@@ -1,6 +1,11 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { LayoutContainer } from '@/components/common/layout-container/layout-container';
 import ManageInventoryPage from './-components/manage-inventory-page';
 
 export const Route = createLazyFileRoute('/manage-inventory/')({
-    component: ManageInventoryPage,
+    component: () => (
+        <LayoutContainer>
+            <ManageInventoryPage />
+        </LayoutContainer>
+    ),
 });
