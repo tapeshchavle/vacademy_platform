17597c37fe872f71921f8b210f4f71a5c174132e
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2026-04-03 22:05:16 +0530

    added fix to issues

--- notification_service/src/main/java/vacademy/io/notification_service/service/WatiService.java
+++ notification_service/src/main/java/vacademy/io/notification_service/service/WatiService.java
@@ -1,6 +1,7 @@
 package vacademy.io.notification_service.service;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
